a082a768cfaa93f8dacad53ea07a0308e547689d
Angel Padilla <angelpadillam@gmail.com>
2026-05-26 04:14:30 -0600

    Actualizado nombre de la aplicacion

--- config/application.rb
+++ config/application.rb
@@ -7,7 +7,7 @@ require "csv"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module Posagenda
+module MiiNegocio
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 8.1
