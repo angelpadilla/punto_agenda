5e6e890a10185930456d26972e1700e273501641
Angel Padilla <angelpadillam@gmail.com>
2026-07-28 20:30:04 -0600

    deshabilitado el rate limit porque uso cloudflare y no me sirve por el momento. ajustes en vistas de registro y login de clientes, ajustes en vistas de terminos y privacidad.

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,7 +1,7 @@
 class Customer < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable,
+  devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :lockable, :trackable
 
   has_many_attached :docs, dependent: :destroy
